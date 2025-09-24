3ade6517512616b8036029b8fb2547a23f1aff4f
Keaton Smith <keaton.smith365@gmail.com>
2025-09-23 23:11:02 -0600

    Dev allowed origins

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+const ALLOWED_DEV_ORIGINS: string[] = process.env.ALLOWED_DEV_ORIGINS ? process.env.ALLOWED_DEV_ORIGINS.split(',') : [];
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -7,6 +9,7 @@ const nextConfig: NextConfig = {
       new URL('https://picsum.photos/**')
     ]
   },
+  allowedDevOrigins: ALLOWED_DEV_ORIGINS,
 };
 
 export default nextConfig;
