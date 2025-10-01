6c9f5967f223af6e263de447875d3406b1a54c0a
Keaton Smith <keaton.smith365@gmail.com>
2025-10-01 17:00:55 -0600

    dynamic skill v3

--- app/components/Skill.tsx
+++ app/components/Skill.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import type { FullSkill } from "@/sanity/SkillManager";
 
 type Props = {
@@ -10,12 +8,12 @@ export default function SkillDetails({ data }: Props) {
   return (
     <div className="relative mt-2 group inline-block">
       {/* Skill button */}
-      <button className="text-black-600 px-3 py-1 rounded-lg bg-blue-100 hover:bg-blue-200 transition">
+      <button className="text-black px-3 py-1 rounded-full bg-blue-100 hover:bg-blue-200 transition">
         {data.name}
       </button>
 
       {/* Overlay on hover */}
-      <div className="absolute left-0 top-full mt-2 w-80 max-w-full bg-white shadow-lg rounded-lg p-4 border border-gray-200 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-200">
+      <div className="absolute left-0 top-full mt-2 w-80 max-w-full bg-white shadow-lg rounded-lg p-4 border border-gray-200 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-200 z-50">
         {data.childrenCategories.map((category) => (
           <div key={category.name} className="mb-4 last:mb-0">
             <h3 className="text-sm font-semibold mb-2">{category.name}</h3>
@@ -31,7 +29,9 @@ export default function SkillDetails({ data }: Props) {
             </div>
           </div>
         ))}
-        {data.childrenCategories.length === 0 && (<span className="text-sm text-gray-500">No more details</span>)}
+        {data.childrenCategories.length === 0 && (
+          <span className="text-sm text-gray-500">No more details</span>
+        )}
       </div>
     </div>
   );
--- app/components/SkillsSection.tsx
+++ app/components/SkillsSection.tsx
@@ -1,19 +1,18 @@
 import { getHighlightedSkills } from "@/sanity/SkillManager";
 import Skill from "./Skill";
 
-
 export default async function SkillsSection() {
   const skills = await getHighlightedSkills();
 
   return (
-    <section id="skills" className="py-20 px-6 max-w-5xl mx-auto text-center">
+    <section id="skills" className="py-20 px-6 max-w-3xl mx-auto text-center">
       <h2 className="text-3xl font-bold">Skills</h2>
-      <div className="mt-12 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
-        {skills.map(
-          (skill) => (
-            <Skill data={skill} key={skill.name} />
-          )
-        )}
+      
+      {/* Flex container for free-form wrapping */}
+      <div className="mt-12 flex flex-wrap justify-center gap-5">
+        {skills.map((skill) => (
+          <Skill data={skill} key={skill.name} />
+        ))}
       </div>
     </section>
   );
